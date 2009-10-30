fc12d106ce8e1ad6709d258fe5b8e6b1fd32ff93
David Wilson dw <David Wilson dw@botanicus.net>
2009-10-30 19:43:56 +0000

    Add initial version of speedup module.

---
+++ _cdblib.c
@@ -0,0 +1,33 @@
+#include "Python.h"
+
+/* Return a Python long instance containing the value of the DJB hash function
+ * for the given string or array. */
+static PyObject *
+djb_hash(PyObject *self, PyObject *args)
+{
+    const unsigned char *s;
+    unsigned int h = 5381;
+    int len;
+
+    if(! PyArg_ParseTuple(args, "t#", &s, &len))
+        return NULL;
+
+    while(len--)
+        h = ((h << 5) + h) ^ *s++;
+
+    return PyLong_FromUnsignedLong((unsigned long) h);
+}
+
+
+static /*const*/ PyMethodDef methods[] = {
+    {"djb_hash", djb_hash, METH_VARARGS,
+     "Return the value of DJB's hash function for the given 8-bit string."},
+    {NULL, NULL, 0, NULL}
+};
+
+
+PyMODINIT_FUNC
+init_cdblib(void)
+{
+    Py_InitModule("_cdblib", methods);
+}
--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from distutils.core import setup, Extension
+
+_cdblib_module = Extension('_cdblib', sources=['_cdblib.c'])
 
 setup(author='David Wilson',
       author_email='dw@botanicus.net',
       description="Pure Python reader/writer for Dan J. Berstein's CDB format.",
       download_url='http://code.google.com/p/python-pure-cdb/',
-      include_package_data=True,
       keywords='cdb file format appengine database db',
       license='MIT',
       name='pure-cdb',
       py_modules=['cdblib'],
-      version='1.0',
-      zip_safe=False)
+      ext_modules=[_cdblib_module],
+      version='1.0'
+)
