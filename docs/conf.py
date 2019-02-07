8c5c0d141779d0326f35a9cc4d028a40d1da01ba
Bo Bayles <bbayles@gmail.com>
2019-02-06 20:53:41 -0600

    Update version information

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'dw, bbayles'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '2.2.0'
+release = '3.0.0'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -24,10 +24,11 @@ setup(
     keywords='cdb file format appengine database db',
     license='MIT',
     name='pure-cdb',
-    version='3.0.0',
+    version='4.0.0',
     packages=find_packages(include=['cdblib']),
     ext_modules=ext_modules,
     install_requires=[],
+    python_requires='>=3.4',
     test_suite='tests',
     tests_require=['flake8'],
     entry_points={
